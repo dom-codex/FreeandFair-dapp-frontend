c2bd566a25f6fd199b1d8ef97c8203b93315907f
dom-codex <dominicwest7@gmail.com>
2022-03-27 03:33:40 +0100

    1 file added

---
+++ src/components/electioncandidates.js
@@ -0,0 +1,42 @@
+import React from 'react';
+const ElectionCandidates = () => {
+  return (
+    <div className={styles.listsofcandidates}>
+      <div className={styles.utilnav}>
+        <div className={candidatesearch}>
+          <input type="text" placeholder="candidate name" />
+          <button>
+            <i class="material-icons">search</i>
+          </button>
+        </div>
+        <div className={styles.addbtncont}>
+          <button>
+            <i class="material-icons">add</i>
+          </button>
+        </div>
+      </div>
+      <div className={styles.candidatescont}>
+        <div className={styles.candidatesitem}>
+          <div>
+            <img src="vote.jpeg" />
+          </div>
+          <div>
+            <div className={styles.candidatedetails}>
+              <p className={styles.label}>Name:</p>
+              <p>Dominic Ibolo .W</p>
+            </div>
+            <div className={styles.candidatedetails}>
+              <p className={styles.label}>Office:</p>
+              <p>President</p>
+            </div>
+            <div className={styles.candidatedetails}>
+              <p className={styles.label}>Mandate:</p>
+              <p>Vision 2022</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ElectionCandidates;
