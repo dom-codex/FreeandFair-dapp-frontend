9954f6ab920665af91ed5cd41ec7b95073a022c7
dom-codex <dominicwest7@gmail.com>
2022-03-31 15:22:45 +0100

    2 files modified

--- src/modals/newpoll.js
+++ src/modals/newpoll.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import styles from ""
 export default NewPollModal = () => {
-  return <section></section>;
+  return (
+    <section className={styles.newpoll}>
+      <div className={styles.modal}>
+        <div className={styles.newmodal}>
+          <div className={styles.submodal}>
+            <div>
+              <label>Poll Title</label>
+              <input type="text" />
+            </div>
+
+            <div>
+              <button>Register</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
