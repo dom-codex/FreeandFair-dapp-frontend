664a14112bb1c1ae54ff4366a14bd9bb143fc04d
dom-codex <dominicwest7@gmail.com>
2022-04-18 00:35:38 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, {createContext,useReducer} from "react";
-import Home from "./pages/home.js";
-import Election  from "./pages/election.js"
-import Electorate from "./pages/electorate.js"
-import Vote from "./pages/vote"
-import {reducer,initialData} from "./utils/logic.js"
-import "./style.css";
-export const AppContext = createContext(null)
+import React, { createContext, useReducer } from 'react';
+import Home from './pages/home.js';
+import Election from './pages/election.js';
+import Electorate from './pages/electorate.js';
+import Vote from './pages/vote';
+import { reducer, initialData } from './utils/logic.js';
+import './style.css';
+export const AppContext = createContext(null);
 export default function App() {
-  const [state,dispatch ] = useReducer(reducer,initialData)
+  const [state, dispatch] = useReducer(reducer, initialData);
   return (
-
-    <AppContext.Provider value={{dispatch,state}}>
-      <Vote/>
+    <AppContext.Provider value={{ dispatch, state }}>
+      <Vote />
     </AppContext.Provider>
   );
 }
--- src/pages/vote.js
+++ src/pages/vote.js
@@ -1,6 +1,11 @@
-import React from "react"
-import Nav from "../components/nav.js"
-const Vote =()=>{
-return <section></section>
-}
+import React from 'react';
+import Nav from '../components/nav.js';
+const Vote = () => {
+  return (
+    <section>
+      <Nav />
+      <div></div>
+    </section>
+  );
+};
 export default Vote;
