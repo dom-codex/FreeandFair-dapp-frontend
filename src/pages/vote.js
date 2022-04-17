b3fa052c85b5e05c443edb88641443863ed42362
dom-codex <dominicwest7@gmail.com>
2022-04-18 00:48:38 +0100

    1 file modified

--- src/pages/vote.js
+++ src/pages/vote.js
@@ -4,7 +4,137 @@ const Vote = () => {
   return (
     <section>
       <Nav />
-      <div></div>
+      <div>
+        <section>
+          <div>
+            <h2 class="election-title">Fupre 2022 General Election</h2>
+            <a href="">Live polls</a>
+            <div class="label">
+              <div class="left"></div>
+              <p>Polls</p>
+              <div class="right"></div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <div class="poll-item">
+              <div class="poll-header">
+                <p>President</p>
+              </div>
+              <div class="poll-candidates">
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item voted">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="poll-item">
+              <div class="poll-header">
+                <p>Vice President</p>
+              </div>
+              <div class="poll-candidates">
+                <div class="poll-candidate-item voted">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+                <div class="poll-candidate-item">
+                  <img src="vote3.jpeg" />
+                  <div class="details">
+                    <p class="candidate-name">Dominic ibolo .w</p>
+                    <p class="candidate-mandate">vision 2022</p>
+                  </div>
+                  <div>
+                    <small>tap to vote</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </section>
   );
 };
