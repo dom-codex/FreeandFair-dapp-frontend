09e9a54fd918b583d945f6095f9b7c8a7d3c229f
dom-codex <dominicwest7@gmail.com>
2022-04-26 11:48:33 +0100

    1 file modified, 1 added

--- src/pages/livepolls.js
+++ src/pages/livepolls.js
@@ -4,7 +4,19 @@ const LivePolls = () => {
   return (
     <section>
       <Nav />
-      <div></div>
+      <div>
+        <section>
+          <div>
+            <h2 class="election-title">Fupre 2022 General Election</h2>
+
+            <div class="label">
+              <div class="left"></div>
+              <p>Polls</p>
+              <div class="right"></div>
+            </div>
+          </div>
+        </section>
+      </div>
     </section>
   );
 };
