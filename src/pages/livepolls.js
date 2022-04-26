0d4d895adf6d0abfa5c7cdb4330739872295e56f
dom-codex <dominicwest7@gmail.com>
2022-04-26 02:54:27 +0100

    1 file added

---
+++ src/pages/livepolls.js
@@ -0,0 +1,11 @@
+import React from 'reat';
+import Nav from '../components/nav.js';
+const LivePolls = () => {
+  return (
+    <section>
+      <Nav />
+      <div></div>
+    </section>
+  );
+};
+export default LivePolls;
