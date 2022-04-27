788b3882927dfc84d7d301873d369a2dbdf79dd2
dom-codex <dominicwest7@gmail.com>
2022-04-27 11:37:39 +0100

    2 files modified, 1 added

---
+++ src/components/electionresult.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from '../styles/livepoll.module.css';
+const ElectionResult = () => {
+  return (
+    <section className={styles.electionresult}>
+      <div className={styles.resultcontainer}>
+        <div className={styles.results}>
+          <h2 className={styles.winnerlabel}>Winners</h2>
+          <div className={styles.resultitem}>
+            <div className={styles.pollheader}>
+              <p>President</p>
+            </div>
+          </div>
+
+          <div className={styles.winner}>
+            <img src="twi.jpg" />
+            <div className={styles.polldetails}>
+              <p>Dominic west</p>
+              <p>Total Votes</p>
+              <p>23,000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className={styles.closebtn}>
+        <i class="material-icons">cancel</i>
+      </button>
+    </section>
+  );
+};
+export default ElectionResult;
--- src/pages/livepolls.js
+++ src/pages/livepolls.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Nav from '../components/nav.js';
 import styles from '../styles/livepoll.module.css';
+import ElectionResult from '../components/electionresult.js';
 const LivePolls = () => {
   return (
     <section>
+      <ElectionResult />
       <Nav />
       <div>
         <section>
@@ -16,6 +18,25 @@ const LivePolls = () => {
               <div className={styles.left}></div>
               <p>Polls</p>
               <div className={styles.right}></div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <div className={styles.pollitem}>
+              <div className={styles.pollheader}>
+                <p>President</p>
+              </div>
+              <div className={styles.pollcandidates}>
+                <div className={styles.pollcandidateitem}>
+                  <img src="vote3.jpeg" />
+                  <div className={`${styles.details} ${styles.livepd}`}>
+                    <p className={styles.candidatename}>Dominic ibolo .w</p>
+                    <p className={styles.candidatevotes}>23,000</p>
+                    <p>votes</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
